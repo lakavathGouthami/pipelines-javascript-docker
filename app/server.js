09a124b6adb3ced0cc46d8f430de46cc66a26490
lakavathGouthami <89766003+lakavathGouthami@users.noreply.github.com>
2021-09-01 18:01:16 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,17 +7,9 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Azure\n');
+  res.send('Hello Azure K8s services \n');
 });
-<p id="demo"></p>
 
-<script>
-var price1 = 5;
-var price2 = 6;
-var total = price1 + price2;
-document.getElementById("demo").innerHTML =
-"The total is: " + total;
-</script>
 
 
 app.listen(PORT, HOST);
