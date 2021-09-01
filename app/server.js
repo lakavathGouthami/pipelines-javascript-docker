f89b91df387b91079c8ce8c7a0e4eac253c417fe
lakavathGouthami <89766003+lakavathGouthami@users.noreply.github.com>
2021-09-01 17:47:06 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,8 +7,18 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello Azure\n');
 });
+<p id="demo"></p>
+
+<script>
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+document.getElementById("demo").innerHTML =
+"The total is: " + total;
+</script>
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
